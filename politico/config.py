3d1d3f9b5f3a8c9a503a6c6560ce5a8e9a3331e8
erycoking <erycoking360@gmail.com>
2019-02-11 09:22:53 +0300

    [starts 163845403] create a route for adding candidate

--- politico/config.py
+++ politico/config.py
@@ -3,14 +3,12 @@
 # import flask
 from flask import Flask
 
-# import user blueprint
+# import blueprints
 from politico.api.v1.user.routes import user
-
-# import party blueprint
 from politico.api.v1.party.routes import party
-
-# import office blueprint
 from politico.api.v1.office.route import office
+from politico.api.v1.candidate.routes import cand
+
 
 
 def create_app():
@@ -18,13 +16,13 @@ def create_app():
     # create the app
     app = Flask(__name__)
 
-    # register user blueprint
-    app.register_blueprint(user, url_prefix='/api/v1')
+    prefix = '/api/v1'
 
-    # register party blueprint
-    app.register_blueprint(party, url_prefix='/api/v1')
+    # register blueprints
+    app.register_blueprint(user, url_prefix=prefix)
+    app.register_blueprint(party, url_prefix=prefix)
+    app.register_blueprint(office, url_prefix=prefix)
+    app.register_blueprint(cand, url_prefix=prefix)
 
-    # register office blueprint
-    app.register_blueprint(office, url_prefix='/api/v1')
 
     return app
