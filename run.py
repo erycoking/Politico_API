2fd902a9a54dff49254eeee603a0e2448f4d2b69
erycoking <erycoking360@gmail.com>
2019-02-15 08:34:42 +0300

    changed run file

--- run.py
+++ run.py
@@ -7,6 +7,6 @@ from politico.config import create_app
 app = create_app()
 
 """Setting debug to true"""
-app.run(debug=False)
+app.run(debug=False, Testing=False)
 # app.run(host=os.getenv('IP', '0.0.0.0'), 
 #             port=int(os.getenv('PORT', 9999)))
