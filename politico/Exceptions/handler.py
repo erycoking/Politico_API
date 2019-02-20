7df31737c856ccc78a71e6cf25e39a7d2cf8276f
erycoking <erycoking360@gmail.com>
2019-02-20 12:21:18 +0300

    [starts #164055166] fixed a bug: registered global errors

--- politico/Exceptions/handler.py
+++ politico/Exceptions/handler.py
@@ -4,9 +4,9 @@ from werkzeug.exceptions import default_exceptions
 error = Blueprint('error_handler', __name__)
 
 @error.app_errorhandler(Exception)
-def handle_error(error):
-    message = [str(x) for x in error.args]
-    status_code = error.status_code
+def handle_error(err):
+    message = [str(x) for x in err.args]
+    status_code = err.status_code
     return make_response(jsonify({
             'status': status_code,
             'error': message
